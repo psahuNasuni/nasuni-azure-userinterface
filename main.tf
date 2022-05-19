4bba9a7360bbfc7041c9cc3fde5280a6b2ac1275
Ubuntu <azureuser@test-azure-acs-vm.apjpz5q3tvuupbv1ml0exbha0d.cx.internal.cloudapp.net>
2022-05-19 09:31:01 +0000

    updated serch-endpoint-test secret value

--- main.tf
+++ main.tf
@@ -5,20 +5,30 @@ resource "random_id" "unique_SearchUI_id" {
 
 resource "null_resource" "install_searchui_web" {
   provisioner "local-exec" {
-          command = "./Install_SearchUI_Web.sh"
-    }
-# depends_on = []
+    command = "./Install_SearchUI_Web.sh"
+  }
+  # depends_on = []
+}
+
+data "azurerm_key_vault" "acs_key_vault" {
+  name                = var.acs_key_vault
+  resource_group_name = var.acs_resource_group
+}
+
+data "azurerm_key_vault_secret" "kvsecret" {
+  name         = "search-endpoint-test"
+  key_vault_id = data.azurerm_key_vault.acs_key_vault.id
 }
 
 resource "null_resource" "update_searchui_js" {
   provisioner "local-exec" {
-          command = "sed -i 's#var volume_api.*$#var volume_api = \"${var.acs_volume_function_url}\"; #g' SearchUI_Web/search.js"
-    }
+    command = "sed -i 's#var volume_api.*$#var volume_api = \"${data.azurerm_key_vault_secret.kvsecret.value}\"; #g' SearchUI_Web/search.js"
+  }
   provisioner "local-exec" {
-          command = "sed -i 's#var search_api.*$#var search_api = \"${var.search_acs_function_url}\"; #g' SearchUI_Web/search.js"
-    }
+    command = "sed -i 's#var search_api.*$#var search_api = \"${data.azurerm_key_vault_secret.kvsecret.value}\"; #g' SearchUI_Web/search.js"
+  }
   provisioner "local-exec" {
-      command = "sudo service apache2 restart"
-    }
-  depends_on = [null_resource.inatall_searchui_web]
+    command = "sudo service apache2 restart"
+  }
+  depends_on = [null_resource.install_searchui_web]
 }

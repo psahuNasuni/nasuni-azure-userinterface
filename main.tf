92303c0fd25dadf107dd085a4444d0da8faae56a
Ubuntu <azureuser@test-azure-acs-vm.apjpz5q3tvuupbv1ml0exbha0d.cx.internal.cloudapp.net>
2022-05-20 11:33:23 +0000

    removed apache restart from local exec

--- main.tf
+++ main.tf
@@ -27,7 +27,4 @@ resource "null_resource" "update_searchui_js" {
   provisioner "local-exec" {
     command = "sed -i 's#var search_api.*$#var search_api = \"${data.azurerm_key_vault_secret.kvsecret.value}\"; #g' SearchUI_Web/search.js"
   }
-  provisioner "local-exec" {
-    command = "sudo service apache2 restart"
-  }
 }

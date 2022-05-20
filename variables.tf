38d804f52c723d3b917810370fdccbb3c26e957c
abedareNasuni <101194763+abedareNasuni@users.noreply.github.com>
2022-05-20 12:44:20 +0530

    Aniket (#1)
    * updated serch-endpoint-test secret value
    
    * Azure SearchUI integration prototype_1
    
    Co-authored-by: Ubuntu <azureuser@test-azure-acs-vm.apjpz5q3tvuupbv1ml0exbha0d.cx.internal.cloudapp.net>
    Co-authored-by: sbhatiaNasuni <sbhatia@nasuni.com>

--- variables.tf
+++ variables.tf
@@ -1,16 +1,14 @@
 
 ################### SearchUI Specific Variables ###################
-variable "git_repo_ui" {
-  description = "git_repo_ui specific to certain repos"
-  default = "nasuni-azure-userinterface"
-}
-variable "search_acs_function_url" {
-  description = "URL of Azure search function that is capable of performing search operation in Azure CognitiveSearch"
-  default = ""
+variable "acs_resource_group" {
+  description = "Resouce group name for Azure Cognitive Search"
+  type        = string
+  default     = ""
 }
 
-variable "acs_volume_function_url" {
-  description = "Azure function url for getting all the Volumes available in Azure CognitiveSearch"
-  default = ""
+variable "acs_key_vault" {
+  description = "Azure Key Vault name for Azure Cognitive Search"
+  type        = string
+  default     = ""
 }
-#########################################
+##################################################################

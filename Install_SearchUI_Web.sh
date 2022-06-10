27c1e2ddcbbf7229f9145ee2b1c1f77c6e136e5b
abedareNasuni <101194763+abedareNasuni@users.noreply.github.com>
2022-06-10 16:20:14 +0530

    changes sequence of copy command (#2)

--- Install_SearchUI_Web.sh
+++ Install_SearchUI_Web.sh
@@ -10,8 +10,8 @@ echo '@@@@@@@@@@@@@@@@@@@@@ STARTED  - Deployment of SearchUI Web Site @@@@@@@@@
 sudo chmod 755 SearchUI_Web/*
 cd SearchUI_Web
 sudo rm -rf /var/www/html/*
-sudo chmod 755 /var/www/html/*
 sudo cp -R * /var/www/html/
+sudo chmod 755 /var/www/html/*
 sudo service apache2 restart
 echo Nasuni ElasticSearch Web portal: http://$(curl ifconfig.me)/index.html
 echo '@@@@@@@@@@@@@@@@@@@@@ FINISHED - Deployment of SearchUI Web Site @@@@@@@@@@@@@@@@@@@@@@@'

4363aaae342fd3e699e152cbe7eb1ea9a302d16e
Pradeepta Kumar Sahu <psahu@nasuni.com>
2022-08-09 19:16:09 +0530

    Added comments and changed resource sequence

--- main.tf
+++ main.tf
@@ -8,18 +8,12 @@ data "azurerm_resource_group" "acs_resource_group" {
   name = var.acs_resource_group
 }
 
-resource "null_resource" "install_searchui_web" {
-  provisioner "local-exec" {
-    command = "./Deploy_SearchUI_Web.sh"
-  }
-  depends_on = [null_resource.update_searchui_js]
-}
-
 data "azurerm_key_vault" "acs_key_vault" {
   name                = var.acs_key_vault
   resource_group_name = data.azurerm_resource_group.acs_resource_group.name
 }
 
+########### Create SEARCH Function ###########
 data "archive_file" "test" {
   type        = "zip"
   source_dir  = "./SearchFunction"
@@ -27,7 +21,7 @@ data "archive_file" "test" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "nasuni-st${random_id.unique_SearchUI_id.hex}"
+  name                     = "nasuni-st-${random_id.unique_SearchUI_id.hex}"
   resource_group_name      = data.azurerm_resource_group.acs_resource_group.name
   location                 = data.azurerm_resource_group.acs_resource_group.location
   account_tier             = "Standard"
@@ -101,6 +95,7 @@ resource "null_resource" "function_app_publish" {
   }
 }
 
+
 resource "azurerm_key_vault_access_policy" "func_vault_id_mngmt" {
   key_vault_id = data.azurerm_key_vault.acs_key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
@@ -125,6 +120,14 @@ resource "null_resource" "set_key_vault_env_var" {
   }
 }
 
+########### Deploy SEARCH UI Web Site ###########
+resource "null_resource" "install_searchui_web" {
+  provisioner "local-exec" {
+    command = "./Deploy_SearchUI_Web.sh"
+  }
+  depends_on = [null_resource.update_searchui_js]
+}
+
 resource "null_resource" "update_searchui_js" {
   provisioner "local-exec" {
     command = "sed -i 's#var search_api.*$#var search_api = \"https://${azurerm_function_app.function_app.default_hostname}/api/search\"; #g' SearchUI_Web/search.js"
@@ -132,6 +135,7 @@ resource "null_resource" "update_searchui_js" {
 
   depends_on = [null_resource.function_app_publish]
 }
+#############################################################
 
 output "FunctionAppSearchURL" {
   value = "https://${azurerm_function_app.function_app.default_hostname}/api/search"

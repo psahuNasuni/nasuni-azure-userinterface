e7c90d02d8be36541eca6be62be7a2caf8910a51
Sachin Gaikwad <sachin.gaikwad@nasuni.com>
2022-09-01 13:35:35 +0530

    Tesing get volume function

--- SearchFunction/get_volume/__init__.py
+++ SearchFunction/get_volume/__init__.py
@@ -12,6 +12,7 @@ def get_volumes(response):
     """
     volume_list = []
     response = json.loads(response.text)
+    logging.info('INFO ::: Insite get_volume Response: {}'.format(response))
     for recordes in response['value']:
         if recordes['volume_name'] not in volume_list:
             volume_list.append(recordes['volume_name'])
@@ -22,6 +23,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     # Connect to an App Configuration store
     connection_string = os.environ["AZURE_APP_CONFIG"]
+    logger.info("INFO ::: Connection String: {}".format(connection_string))
     app_config_client = AzureAppConfigurationClient.from_connection_string(connection_string)
 
     # Set the Azure Cognitive Search Variables
@@ -34,6 +36,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     acs_api_key = retrieved_config_acs_api_key.value
     nmc_api_acs_url = retrieved_config_nmc_api_acs_url.value
 
+    logging.info('INFO ::: acs_api_key:{}'.format(acs_api_key))
+    logging.info('INFO ::: nmc_api_acs_url:{}'.format(nmc_api_acs_url))
     # Define the names for the data source, skillset, index and indexer
     index_name = "index"
     logging.info('Setting the endpoint')
@@ -48,9 +52,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Searching URl")
     r = requests.get(endpoint + "/indexes/" + index_name +
                 "/docs?&search=*", headers=headers, params=params)
-    
-    r = get_volumes(r)
 
+    logging.info('INFO ::: Request Json:{}'.format(r))
+    r = get_volumes(r)
+    logging.info('INFO ::: Volume List:{}'.format(r))
     response = {
             "statusCode": 200,
             "headers": {

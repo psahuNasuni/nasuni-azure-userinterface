cb23e1e231417ead082f0260a81895a01b315820
sbhatiaNasuni <sbhatia@nasuni.com>
2022-09-09 17:17:57 +0530

    Deployment of Webserver and apache configuration setup

--- Deploy_SearchUI_Web.sh
+++ Deploy_SearchUI_Web.sh
@@ -7,11 +7,14 @@ sudo ufw allow 'Apache'
 sudo service apache2 restart
 echo '@@@@@@@@@@@@@@@@@@@@@ FINISHED - Install WEB Server             @@@@@@@@@@@@@@@@@@@@@@@'
 echo '@@@@@@@@@@@@@@@@@@@@@ STARTED  - Deployment of SearchUI Web Site @@@@@@@@@@@@@@@@@@@@@@@'
-sudo chmod 755 SearchUI_Web/*
-cd SearchUI_Web
-sudo rm -rf /var/www/html/*
-sudo cp -R * /var/www/html/
-sudo chmod 755 /var/www/html/*
+sudo chmod -R 755 /var/www
+sudo cp -r SearchUI_Web /var/www/.
+sudo cp -r Tracker_UI /var/www/.
+sudo rm -rf /var/www/html/index.html
+sudo chmod 755 WebConfig.sh
+sudo ./WebConfig.sh
 sudo service apache2 restart
-echo Nasuni Cognitive Search Web portal: http://$(curl ifconfig.me)/index.html
+sudo service apache2 restart
+echo Nasuni Cognitive Search Web portal: http://$(curl ifconfig.me)/search
+echo Nasuni Cognitive TrackerUI Web portal: http://$(curl ifconfig.me)/tracker
 echo '@@@@@@@@@@@@@@@@@@@@@ FINISHED - Deployment of SearchUI Web Site @@@@@@@@@@@@@@@@@@@@@@@'

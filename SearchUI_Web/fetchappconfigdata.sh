bc71d9aa1b03a9e6b28d969066e37c30abac3d23
Arpit Jain <ajain@nasuni.com>
2023-12-06 18:26:46 +0530

    update absolute path in fetchappconfig script

--- SearchUI_Web/fetchappconfigdata.sh
+++ SearchUI_Web/fetchappconfigdata.sh
@@ -5,4 +5,4 @@ result=$(az appconfig kv list --name $1 --key $2)
 
 formatted_result=$(echo "$result" | jq -r 'map({(.value): .label}) | add')
 
-echo "$formatted_result" > sso.json
+echo "$formatted_result" > /var/www/SearchUI_Web/sso.json
